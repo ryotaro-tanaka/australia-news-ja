54c4c5a733d0a7ab6d54310200661ff3047ec516
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2026-04-27 16:03:09 +1000

    fix: remove unused React import to resolve TS6133 error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { NewsProvider, useNewsContext } from './state/NewsContext'
 import { NewsList } from './components/news/NewsList'
