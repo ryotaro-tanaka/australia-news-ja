9559570c069aea5db674615293154b3dcb13ade4
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2026-04-27 14:27:02 +1000

    style: remove unnecessary header from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import './App.css'
-import { NewsProvider, useNewsContext } from './state/NewsContext'
+import { NewsProvider } from './state/NewsContext'
 import { NewsList } from './components/news/NewsList'
 
 function NewsApp() {
-  const { state: { language } } = useNewsContext();
-  const title = language === 'id' ? 'Berita Australia' : '南半球の朝ごはんニュース';
-  
   return (
     <div className="container">
-      <header>
-        <h1>{title}</h1>
-      </header>
       <main>
         <NewsList />
       </main>
