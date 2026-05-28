4407a2059cd29c64be3d3745d61ea296203dfd27
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2026-05-28 11:55:10 +1000

    feat: remove /id specific middleware to simplify routing

--- functions/_middleware.ts
+++
@@ -1,7 +0,0 @@
-export const onRequest: PagesFunction = async (context) => {
-  const url = new URL(context.request.url);
-  if (url.pathname === '/id' || url.pathname.startsWith('/id/')) {
-    return new Response('Not Found', { status: 404 });
-  }
-  return context.next();
-};
